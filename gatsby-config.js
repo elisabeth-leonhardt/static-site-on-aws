dd5f200c37370cc4a7accd612774999d47229e47
Elisabeth Leonhardt <elisabeth.ingcomp@gmail.com>
2023-03-27 09:31:57 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `my-static-site-on-aws`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
